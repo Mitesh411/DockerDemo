f15eb17ce05ceaed7a7ba35880e0e3c23839439e
Mitesh411 <miteshdandade@ymail.com>
2024-02-20 17:48:14 +0530

    Added New Test Case

--- src/test/java/org/miteshdandade/GoogleHomePageTest.java
+++ src/test/java/org/miteshdandade/GoogleHomePageTest.java
@@ -1,8 +1,6 @@
 package org.miteshdandade;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
