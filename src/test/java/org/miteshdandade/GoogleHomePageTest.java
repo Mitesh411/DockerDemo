2a1f0c9e7cf5d40bfafffc140f8651b4c43dcdd9
Mitesh411 <miteshdandade@ymail.com>
2024-02-20 17:45:34 +0530

    Added New Test Case

--- src/test/java/org/miteshdandade/GoogleHomePageTest.java
+++ src/test/java/org/miteshdandade/GoogleHomePageTest.java
@@ -1,6 +1,8 @@
 package org.miteshdandade;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -28,6 +30,17 @@ public class GoogleHomePageTest {
         String getTitle = driver.getTitle();
         Assert.assertEquals(getTitle, "Google");
     }
+
+    @Test
+    public void verifyGoogleSearchResult(){
+        driver.manage().window().maximize();
+        driver.navigate().to(appURL);
+
+        System.out.println("Current URL is:" + driver.getCurrentUrl());
+
+        System.out.println("Current Title is:" + driver.getTitle());
+    }
+
 
     @AfterClass
     public void tearDown() {
