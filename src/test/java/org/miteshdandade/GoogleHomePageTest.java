708e2fa40267d9a47d8082ce7f02de9a8ac6e8c5
Mitesh411 <miteshdandade@ymail.com>
2024-02-20 17:07:04 +0530

    Initial commit

---
+++ src/test/java/org/miteshdandade/GoogleHomePageTest.java
@@ -0,0 +1,36 @@
+package org.miteshdandade;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleHomePageTest {
+    private WebDriver driver;
+    String appURL = "http://google.com";
+
+    @BeforeClass
+    public void testSetUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+         driver = new ChromeDriver(options);
+     //   driver = new ChromeDriver();
+    }
+
+    @Test
+    public void verifyGooglePageTittle() {
+        driver.manage().window().maximize();
+        driver.navigate().to(appURL);
+
+        String getTitle = driver.getTitle();
+        Assert.assertEquals(getTitle, "Google");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
